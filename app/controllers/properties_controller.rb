db48b07997195ba986cd0278ed7e8ba7acf2f35b
rails <rails@rails.(none)>
2014-05-14 17:17:39 +0530

    small change

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -79,19 +79,6 @@ class PropertiesController < ApplicationController
       end
     else
       @properties_map = Property.all
-      @lat_longs = []
-      @types = []
-      @property_types = params[:search][:property_type]
-      @contents = []
-      @properties_map.each do |prop|
-        @lat_longs << [prop.latitude,prop.longitude]
-        @types << [prop.property_type.split('/')[0]]
-        if prop.property_type=="Apartment" or prop.property_type=="Villa" or prop.property_type=="Single Home" or prop.property_type=="Family House"
-          @contents << ['<div class="infobox clearfix"><div class="close"><img src="/assets/img/cross.png" alt=""></div><div class="image"><a href='"properties/#{prop.id}"'><img src='"#{prop.images.present? ? prop.images.first.image : " "}"' alt='"#{prop.title}"' width=100 height=100></a></div><div class="info"><div class="title"><a href='"properties/#{prop.id}"'>'"#{prop.title}"'</a></div><div class="location">'"#{prop.location}"'</div><div class="property-info clearfix"><div class="area"><i class="icon icon-normal-cursor-scale-up"></i>'"#{prop.area}"'m<sup>2</sup></div><div class="bedrooms"><i class="icon icon-normal-bed"></i>'"#{prop.no_of_rooms}"'</div><div class="bathrooms"><i class="icon icon-normal-shower"></i>'"#{prop.bath_rooms}"'</div></div><div class="price">'"#{prop.price}"' INR</div><div class="link"><a href="properties/'"#{prop.id}"'">View more</a></div></div></div>']
-        else
-          @contents << ['<div class="infobox clearfix"><div class="close"><img src="/assets/img/cross.png" alt=""></div><div class="image"><a href='"properties/#{prop.id}"'><img src='"#{prop.images.present? ? prop.images.first.image : " "}"' alt='"#{prop.title}"' width=100 height=100></a></div><div class="info"><div class="title"><a href='"properties/#{prop.id}"'>'"#{prop.title}"'</a></div><div class="location">'"#{prop.location}"'</div><div class="property-info clearfix"><div class="area"><i class="icon icon-normal-cursor-scale-up"></i>'"#{prop.area}"'m<sup>2</sup></div></div><div class="price">'"#{prop.price}"' INR</div><div class="link"><a href="properties/'"#{prop.id}"'">View more</a></div></div></div>']
-        end 
-      end
     end
     @contact = Contact.new
   end
